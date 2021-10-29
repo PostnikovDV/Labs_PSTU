f9e543d99d012c623ea2e4b9ff15d7bc6e72edd9
PostnikovDV <92156834+PostnikovDV@users.noreply.github.com>
2021-10-29 21:10:33 +0500

    Add files via upload

---
+++ олимпиадная задача №2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int i, h, g;
+	srand(time(0));
+	i = 1 + rand() % 100;
+	cout << "Введите число: ";
+	cin >> h;
+	
+	for (g = 0; g < 6; g++)
+	{
+		if (h < 1 || h >100)
+		{
+			cout << "Введите число еще раз: ";
+			cin >> h;
+		}
+		
+	    
+		if (h > i)
+		{
+			cout << "Загаданное число меньше введенного  \n";
+			cin >> h;
+		}
+		else if (h < i)
+		{
+			cout << "Загаданное число больше введенного  \n";
+			cin >> h;
+		}
+		
+	}
+	if(h == i)
+		cout << "вы угадали число " << '(' << i << ')';
+	else
+	cout << "вы  не угадали число за 7 попыток " <<"загадонное число - " <<'(' << i << ')';
+	return 0;
+}
