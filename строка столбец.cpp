b2ad57e63a9b5c1d36d403c418b13440e80037ab
PostnikovDV <92156834+PostnikovDV@users.noreply.github.com>
2022-02-12 14:54:37 +0500

    Add files via upload

---
+++ строка столбец.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(0));
+	int const N = 4;
+	int arr[N][N], k, p, t, z, y, x;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			arr[i][j] = rand() % 100 - 1;
+			cout << arr[i][j] << '\t';
+		}
+		cout << endl;
+	}
+	cout << "Введите номер строки и столбца: ";
+	cin >> k >> p;
+	z = arr[k - 1][k - 1];
+	//int y = arr[p-1][p-1];
+	x = arr[k - 1][p - 1];// элемент на пересечении
+	for (int i = 0; i < N; i++)
+	{
+		if (i != k - 1 && i != p - 1)
+		{
+			t = arr[k - 1][i];
+			arr[k - 1][i] = arr[i][p - 1];
+			arr[i][p - 1] = t;
+		}
+		else
+			arr[i][i] = x;
+	}
+	arr[k - 1][p - 1] = z;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+			cout <<arr[i][j] << '\t';
+		cout << endl;
+	}
+	return 0;
+}
